1f5ecdb8627c3348cd12f2db43ab1408027d6f81
4493fabius@163.com <4493fabius@163.com>
2018-06-26 11:30:17 +0800

    Initial Commit

---
+++ UseGitFlow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UseGitFlow
+//
+//  Created by  玉峰 邓 on 2018/6/26.
+//  Copyright © 2018年 FZFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

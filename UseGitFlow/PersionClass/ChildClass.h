eabac9fc81cc86b813023cdb685cc92a94d1889f
4493fabius@163.com <4493fabius@163.com>
2018-06-26 11:34:32 +0800

    add person class folder and childclass

---
+++ UseGitFlow/PersionClass/ChildClass.h
@@ -0,0 +1,13 @@
+//
+//  ChildClass.h
+//  UseGitFlow
+//
+//  Created by  玉峰 邓 on 2018/6/26.
+//  Copyright © 2018年 FZFeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChildClass : NSObject
+
+@end
---
+++ UseGitFlow/PersionClass/ChildClass.m
@@ -0,0 +1,13 @@
+//
+//  ChildClass.m
+//  UseGitFlow
+//
+//  Created by  玉峰 邓 on 2018/6/26.
+//  Copyright © 2018年 FZFeng. All rights reserved.
+//
+
+#import "ChildClass.h"
+
+@implementation ChildClass
+
+@end
